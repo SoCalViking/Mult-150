7ffb796992b975628b7d5ad1d69fffffb2304568
SoCalViking <magekrew@gmail.com>
2024-10-28 11:26:23 -0700

    late

---
+++ kburton_Hour8and21/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    int healthpoints = 3992
+    void Start()
+    {
+        UsePotion(healthpoints);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    int UsePotion(health)
+    {
+        return health + 400;
+    }
+}
