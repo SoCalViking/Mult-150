785edabe8d5e3e0a5d06eebd370867924877ee6a
SoCalViking <magekrew@gmail.com>
2024-11-12 14:26:12 -0800

    question update

--- kburton_Hour10/Assets/GameCritic.cs
+++ kburton_Hour10/Assets/GameCritic.cs
@@ -3,4 +3,19 @@ Variations to Chaos Balls
 Changed the bumper shape
 Change the footsteps audio 
 Covering up terrain with other game objects
- */
+ 
+Improving the game questions
+Is the game too easy or hard? 
+both version of Chaos Ball are too easy. Often the balls find their goals without the player doing anything. 
+
+What would make it easier or Harder?
+To increase the challenge, add more buffer/walls around the colored goal or shrink the size of the goal. 
+
+What would give it a wow factor? 
+Adding more visual effects when the ball enters the goal. A new skin or theme to emerse the player in the games fantasy. Backgorund music wouldn't hurt either. 
+
+WHat parts of the game is fun? What parts of the game are tedious? 
+The idea of the game is fun but lacks much reply value. The chaos ball doesn't really do much to enhance the play experience. The square play zone is boring. 
+
+
+*/
